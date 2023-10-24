178116b7ffc79d57a11726123196d4f505c16ea5
antonio <antonio.cardinali@hotmail.it>
2023-10-24 20:53:05 +0200

    Update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,
-  user_name: ENV["USERNAME"],
+  user_name: ENV["USERNOME"],
   password: ENV["PASSWORD"],
   authentication: 'plain',
   enable_starttls_auto: true
